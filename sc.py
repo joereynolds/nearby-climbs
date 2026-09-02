a660900b4e9c7c178b485b96cdc66eacdcf47585
Joe Reynolds <joereynolds952@gmail.com>
2026-09-02 12:40:48 +0100

    Add limit and radius args

--- sc.py
+++ sc.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import json
 import math
 import urllib.request
@@ -7,15 +8,41 @@ import urllib.request
 location_url = "https://ipinfo.io/json"
 overpass_url = "https://maps.mail.ru/osm/tools/overpass/api/interpreter"
 
-radius = 15.0
-result_limit = 20
+
+parser = argparse.ArgumentParser(
+    prog="sc",
+    description="Find local climbs",
+)
+
+parser.add_argument(
+    '-l',
+    '--limit',
+    help="How many climbs to return",
+    default=20,
+)
+
+parser.add_argument(
+    '-r',
+    '--radius',
+    help="The search radius from your location in miles",
+    default=15
+)
+
+parser.add_argument(
+    '-v',
+    '--verbose',
+)
+
+print('done')
+exit()
+args = parser.parse_args()
 
 location_data = json.loads(urllib.request.urlopen(location_url).read())
 
 lat, lon = map(float, location_data["loc"].split(","))
 
-lat_adjustment = radius / 69
-lon_adjustment = radius / (69 * math.cos(math.radians(lat)))
+lat_adjustment = args.radius / 69
+lon_adjustment = args.radius / (69 * math.cos(math.radians(lat)))
 
 min_lat, max_lat = round(lat - lat_adjustment, 3), round(lat + lat_adjustment, 3)
 min_lon, max_lon = round(lon - lon_adjustment, 3), round(lon + lon_adjustment, 3)
@@ -27,7 +54,7 @@ query = f"""
 (
   nwr["sport"="climbing"][name]{bbox};
 );
-out tags {result_limit};
+out tags {args.limit};
 """
 
 data = query.encode("utf-8")
@@ -42,8 +69,6 @@ with urllib.request.urlopen(request) as response:
     print(result)
 
 # Improvements:
-# take a --limit param
-# take a --radius param
 # take a --location param
 #
 # Cache each call and save in XDG_DATA_HOME.
@@ -52,4 +77,3 @@ with urllib.request.urlopen(request) as response:
 # result would get cached and saved in XDG_DATA_HOME so we don't hammer the API
 #
 # Link the OSM url or something
-
